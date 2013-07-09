90276615663f9591368c2f97fa5bce34e0744221
Ruben Orta <ruben.orta@gmail.com>
2013-07-09 17:35:15 +0200

    Adding index.html support

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  fs.readFile('index.html','ascii', function(err, data) {
+    if (err) throwv err;
+    console.log(data.toString());
+    response.send(data.toString());
+  });
 });
 
 var port = process.env.PORT || 5000;
